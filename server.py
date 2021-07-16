309ccc2eea8fc9d24c10284172b9ba219b27b23d
somir1 <leonsomir@gmail.com>
2021-07-16 17:09:52 -0500

    user CR to now users CRUD

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from logging import debug
 from flask import Flask, render_template, request, redirect
+from werkzeug import datastructures
 from users import User
 
 app = Flask(__name__)
@@ -19,8 +20,48 @@ def adNew():
 @app.route('/users', methods = ['POST'])
 def create_user():
     print('post user')
+    user_id = User.create_user(request.form)
+    print(user_id)
+    return redirect('/showuser/'+ str(user_id) + '/show')
+
+@app.route('/users/<int:user_id>/delete')
+def delete_theuser(user_id):
+    data = {
+        'id' : user_id
+    }
+
+    User.delete_DAuser(data)
+    return redirect('/')
+
+@app.route('/showuser/<int:user_id>/show')
+def show_new(user_id):
+    data = {
+        'id' : user_id
+    }
+    user = User.show_user(data)
+    print(user)
+    return render_template('showuser.html', user = user)
+
+@app.route('/edituser/<int:user_id>/edit')
+def edit_user(user_id):
     
-    User.create_user(request.form)
+    data = {
+        'id' : user_id
+    }
+
+    user = User.show_user(data)
+    return render_template('edit.html', user = user)
+
+@app.route('/updateuser', methods = ['POST'])
+def update_user():
+    print(request.form['id'])
+    data = {
+        'id' : request.form['id'],
+        'first_name' : request.form['first_name'],
+        'last_name' : request.form['last_name'],
+        'email' : request.form['email']
+    }
+    User.edit_theuser(data)
     return redirect('/')
 
 if __name__ == '__main__':
--- users.py
+++ users.py
@@ -22,3 +22,24 @@ class User():
         query = "INSERT INTO users(first_name, last_name, email) VALUES (%(first_name)s, %(last_name)s, %(email)s);"
 
         return connectToMySQL('users_schema').query_db(query, data)
+
+    @classmethod
+    def delete_DAuser(cls, data):
+        query = "DELETE FROM users WHERE id = %(id)s;"
+
+        return connectToMySQL('users_schema').query_db(query, data)
+
+    @classmethod
+    def show_user(clas, data):
+        query = "SELECT * FROM users WHERE id = %(id)s;"
+        
+        # return the array of result
+        results = connectToMySQL('users_schema').query_db(query, data)
+
+        return results[0]
+
+    @classmethod
+    def edit_theuser(clas, data):
+
+        query = " UPDATE users SET first_name = %(first_name)s , last_name = %(last_name)s, email = %(email)s WHERE id = %(id)s;"
+        return connectToMySQL('users_schema').query_db(query, data)
